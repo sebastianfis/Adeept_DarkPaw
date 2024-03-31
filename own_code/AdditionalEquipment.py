587ba1d49b4ae9149dc9c68261fa3e86ae970ee4
Sebastian <sebastian_fis@yahoo.de>
2024-03-31 20:02:01 +0200

    - Updated Distance sensor class, implemented LED class

--- own_code/AdditionalEquipment.py
+++ own_code/AdditionalEquipment.py
@@ -1,9 +1,20 @@
 from mpu6050 import mpu6050
 import time
 import RPi.GPIO as GPIO
+from rpi_ws281x import Adafruit_NeoPixel, Color
+from threading import Event
 
 # GPIO Modus (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
+
+# LED strip configuration:
+LED_COUNT      = 3      # Number of LED pixels.
+LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 class AccSensor:
     def __init__(self):
@@ -41,9 +52,13 @@ class AccSensor:
 
 
 class DistSensor:
-    def __init__(self,GPIO_trigger: int=18, GPIO_echo: int=24):
+    def __init__(self, GPIO_trigger: int = 18, GPIO_echo: int = 24, cont_measurement_timer: int = 100):
         self.trigger = GPIO_trigger
         self.echo = GPIO_echo
+        self.last_measurement = 0
+        self.cont_measurement_timer = cont_measurement_timer
+        self.cont_measurement_flag = Event()
+        self.cont_measurement_flag.clear()
         # Richtung der GPIO-Pins festlegen (IN / OUT)
         GPIO.setup(self.trigger, GPIO.OUT)
         GPIO.setup(self.echo, GPIO.IN)
@@ -83,3 +98,66 @@ class DistSensor:
         except KeyboardInterrupt:
             print("Messung vom User gestoppt")
             GPIO.cleanup()
+
+    def measure_cont(self):
+        last_exec_time = 0
+        while self.cont_measurement_flag.is_set():
+            now_time = time.perf_counter_ns()
+            if (now_time - last_exec_time) > 1e6 * self.cont_measurement_timer:
+                self.last_measurement = self.take_measurement()
+                last_exec_time = now_time
+
+
+class LED:
+    def __init__(self, led_pin: int = 5):
+        self.led_count = 7      # Number of LED pixels.
+        self.led_pin = led_pin      # GPIO pin connected to the pixels (18 uses PWM!).
+        self.led_freq_hz = 800000  # LED signal frequency in hertz (usually 800khz)
+        self.led_dma = 10      # DMA channel to use for generating signal (try 10)
+        self.led_brightness = 255     # Set to 0 for darkest and 255 for brightest
+        self.led_invert = False   # True to invert the signal (when using NPN transistor level shift)
+        self.led_channel = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+        # Create NeoPixel object with appropriate configuration.
+        self.strip = Adafruit_NeoPixel(self.led_count, self.led_pin, self.led_freq_hz, self.led_dma, self.led_invert, self.led_brightness, self.led_channel)
+        # Intialize the library (must be called once before other functions).
+        self.strip.begin()
+
+        self.colorBreathR = 0
+        self.colorBreathG = 0
+        self.colorBreathB = 0
+        self.breathSteps = 10
+
+        self.lightMode = 'none'  # 'none' 'police' 'breath'
+
+        GPIO.setwarnings(False)
+        GPIO.setup(self.led_pin, GPIO.OUT)
+
+
+    # Define functions which animate LEDs in various ways.
+    def setColor(self, R, G, B):
+        """Wipe color across display a pixel at a time."""
+        color = Color(int(R), int(G), int(B))
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+
+    def light_off(self):
+        self.setColor(0, 0, 0)
+
+    def all_good(self):
+        self.setColor(0, 0, 255)
+
+    def yellow_alert(self):
+        self.setColor(255, 191, 0)
+
+    def red_alert(self):
+        self.setColor(255, 0, 0)
+
+    def setSomeColor(self, R, G, B, ID):
+        color = Color(int(R), int(G), int(B))
+        # print(int(R),'  ',int(G),'  ',int(B))
+        for i in ID:
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+
