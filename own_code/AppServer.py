4566dc84fb3a00dce4d1211f7b26bedbd49cf1a2
Sebastian <sebastian_fis@yahoo.de>
2024-11-21 23:59:52 +0100

    - Corrected error in index javascript

--- own_code/AppServer.py
+++ own_code/AppServer.py
@@ -19,19 +19,24 @@ logger = logging.getLogger(__name__)
 
 # for triggering the shutdown procedure when a signal is detected
 keyboard_trigger = Event()
+
+
 def signal_handler(signal, frame):
     logging.info('Signal detected. Stopping threads.')
     keyboard_trigger.set()
 
+
 #######################
-### Web Server Stuff ##
+#   Web Server Stuff  #
 #######################
+
 
 HOST = "0.0.0.0"
 WEB_PORT = 4664
 app = Flask(__name__, static_url_path='')
 directory_path = '/home/pi/Adeept_DarkPaw/own_code/static/'
 command_queue = queue.Queue()
+
 
 class WebServerThread(Thread):
     """
@@ -53,10 +58,12 @@ class WebServerThread(Thread):
         logging.info('Stopping Flask server')
         self.srv.shutdown()
 
+
 @app.route('/')
 def index():
     """Render the index.html template on the root URL."""
     return render_template('index.html')
+
 
 @app.route('/process_button_click/<command_string>')
 def process_button_click(command_string):
@@ -64,9 +71,11 @@ def process_button_click(command_string):
     # print('command received:' + command_string)
     return Response()
 
+
 @app.route("/<string:page_name>")
 def page(page_name):
     return render_template("{}".format(page_name))
+
 
 @app.route("/static/<path:path>")
 def send_static(path):
@@ -74,7 +83,7 @@ def send_static(path):
 
 
 #############################
-### Video Streaming Stuff ###
+#   Video Streaming Stuff   #
 #############################
 
 class StreamingOutput(io.BufferedIOBase):
@@ -87,14 +96,15 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
+
 class StreamingHandler(server.BaseHTTPRequestHandler):
     """
     Implementing GET request for the video stream.
     """
-    def do_GET(self, output: StreamingOutput):
+    def do_GET(self):
         if self.path == '/stream.mjpg':
             self.send_response(200)
-            self.send_header('Age', 0)
+            self.send_header('Age', '0')
             self.send_header('Cache-Control', 'no-cache, private')
             self.send_header('Pragma', 'no-cache')
             self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
@@ -118,9 +128,11 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.end_headers()
 
+
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
+
 
 if __name__ == '__main__':
     # registering both types of signals
@@ -132,9 +144,10 @@ if __name__ == '__main__':
     camera.configure(camera.create_video_configuration(main={"size": (800, 600)}, lores={}))
     encoder = MJPEGEncoder()
     camera.start()
-
     time.sleep(1)
-    camera.start_recording(MJPEGEncoder(), FileOutput(StreamingOutput()))
+
+    output = StreamingOutput()
+    camera.start_recording(MJPEGEncoder(), FileOutput(output))
     logging.info("Started recording with picamera")
     STREAM_PORT = 4665
     stream = StreamingServer((HOST, STREAM_PORT), StreamingHandler)
@@ -165,70 +178,3 @@ if __name__ == '__main__':
     webserver.join()
     streamserver.join()
     logging.info("Stopped all threads")
-
-
-#
-# class WebInterface:
-#     def __init__(self) -> None:
-#         # initialize a flask object
-#         self.app = Flask(__name__)
-#         self.sio_instance = SocketIO(self.app)
-#         self.register_endpoints()
-#         # initialize the output frame and a lock used to ensure thread-safe
-#         # exchanges of the output frames (useful when multiple browsers/tabs
-#         # are viewing the stream)
-#         self.outputFrame = None
-#         self.lock = threading.Lock()
-#         # initialize the video stream and allow the camera sensor to
-#         # warmup
-#         time.sleep(2.0)
-#
-#     def register_endpoints(self):
-#         self.app.add_url_rule("/", view_func=self.index)
-#         self.app.add_url_rule("/index", view_func=self.index)
-#         # self.app.add_url_rule("/video_feed", view_func=self.video_feed)
-#         self.app.add_url_rule("/process_button_click/<command_string>", view_func=self.process_button_click)
-#
-#     @staticmethod
-#     def index():
-#         # return the rendered template
-#         return render_template("index.html")
-#
-#     def video_feed(self):
-#         # return the response generated along with the specific media
-#         # type (mime type)
-#         # return Response(self.generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
-#         self.sio_instance.emit('send', self.generate())
-#
-#     @staticmethod
-#     def process_button_click(command_string):
-#         print('command received:' + command_string)
-#         return Response()
-#
-#     def generate(self):
-#         # loop over frames from the output stream
-#         while True:
-#             # wait until the lock is acquired
-#             with self.lock:
-#                 # check if the output frame is available, otherwise skip
-#                 # the iteration of the loop
-#                 if self.outputFrame is None:
-#                     continue
-#                 # encode the frame in JPEG format
-#                 (flag, frame) = cv2.imencode(".jpg", self.outputFrame)
-#                 # encodedImage = self.outputFrame
-#                 # ensure the frame was successfully encoded
-#                 if not flag:
-#                     continue
-#             # yield the output frame in the byte format
-#             yield base64.encodebytes(frame[1].tobytes()).decode("utf-8")
-#             # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-#             #        bytearray(encodedImage) + b'\r\n')
-#
-#     def run(self, ip_adress, port, debug=True, threaded=True, use_reloader=False):
-#         self.app.run(ip_adress, port, debug=debug, threaded=threaded, use_reloader=use_reloader)
-#
-#
-# if __name__ == "__main__":
-#     app = WebInterface()
-#     app.run(ip_adress="0.0.0.0", port=4664)
