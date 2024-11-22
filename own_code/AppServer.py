27dd56e40346a17f60366ab11766dfd57fee9fd9
Sebastian <sebastian_fis@yahoo.de>
2024-11-22 22:41:37 +0100

    - Added White balance configuration

--- own_code/AppServer.py
+++ own_code/AppServer.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
 
     # firing up the video camera (pi camera)
     camera = Picamera2()
-    camera.set_controls({"AwbMode": controls.AwbModeEnum.Indoor})
+    camera.set_controls({"AwbMode": controls.AwbModeEnum.Auto})
     camera.configure(camera.create_video_configuration(main={"size": (800, 600)}, lores={}))
     camera.start()
     time.sleep(1)
