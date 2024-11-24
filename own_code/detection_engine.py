5208dd3be3d834c86372db5df09de88effb7706a
Sebastian <sebastian_fis@yahoo.de>
2024-11-24 12:37:03 +0100

    - Updated handling of empty boxes array

--- own_code/detection_engine.py
+++ own_code/detection_engine.py
@@ -141,8 +141,8 @@ def main() -> None:
     while True:
         if not results_queue.empty():
             result_frame = results_queue.get()
-            cv2.imshow("Object detection",result_frame)
-            cv2.waitKey(0)
+            cv2.imshow("Object detection", result_frame)
+            # cv2.waitKey(0)
 
 if __name__ == "__main__":
     main()
