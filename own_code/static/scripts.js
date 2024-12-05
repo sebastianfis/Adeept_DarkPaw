e97d20f0ece9ef2fb6d3a361e756d7f74dbb450c
Sebastian <sebastian_fis@yahoo.de>
2024-12-05 18:34:14 +0100

    - started integration of velocity setting

--- own_code/static/scripts.js
+++ own_code/static/scripts.js
@@ -10,12 +10,23 @@ function Set_actuator() {
     command = 'setpwm_' + act_number + ':' + set_value;
     Btn_Click(command);
 }
+//
+//const velocity_value = document.querySelector("#velocity_value");
+//const velocity_input = document.querySelector("#velocity_slider");
+//velocity_value.textContent = velocity_input.value;
+//velocity_input.addEventListener("velocity_input", (event) => {
+//  velocity_value.textContent = event.target.value + " %";
+//  fetch('/process_velocity_change/'+ event.target.value)
+//        .then(request => request.text(event.target.value))
+//});
 
-const velocity_value = document.querySelector("#velocity_value");
-const velocity_input = document.querySelector("#velocity_slider");
-velocity_value.textContent = velocity_input.value;
-velocity_input.addEventListener("velocity_input", (event) => {
-  velocity_value.textContent = event.target.value + " %";
-  fetch('/process_velocity_change/'+ event.target.value)
-        .then(request => request.text(event.target.value))
-});
+var slider = document.getElementById("velocity_slider");
+var output = document.getElementById("velocity_value");
+output.value = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.value = this.value;
+  fetch('/process_velocity_change/'+ this.value)
+        .then(request => request.text(this.value))
+}
