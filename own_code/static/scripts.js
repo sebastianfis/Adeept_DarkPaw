46e495714b1f4361e9bb06373f71746223098d9f
Sebastian <sebastian_fis@yahoo.de>
2024-12-06 18:28:30 +0100

    - started integration of velocity setting

--- own_code/static/scripts.js
+++ own_code/static/scripts.js
@@ -10,30 +10,24 @@ function Set_actuator() {
     command = 'setpwm_' + act_number + ':' + set_value;
     Btn_Click(command);
 }
-//
-//const velocity_value = document.querySelector("#velocity_value");
-//const velocity_input = document.querySelector("#velocity_slider");
-//velocity_value.textContent = velocity_input.value;
-//velocity_input.addEventListener("velocity_input", (event) => {
-//  velocity_value.textContent = event.target.value + " %";
-//  fetch('/process_velocity_change/'+ event.target.value)
-//        .then(request => request.text(event.target.value))
-//});
 
-//var slider = document.getElementById("velocity_slider");
-//var output = document.getElementById("velocity_value");
-//output.value = slider.value; // Display the default slider value
-//
-//// Update the current slider value (each time you drag the slider handle)
-//slider.oninput = function() {
-//  output.value = this.value;
-//  fetch('/process_velocity_change/'+ this.value)
-//        .then(request => request.text(this.value))
-//}
+var slider = document.getElementById("velocity_slider");
 
-document.getElementById("velocity_slider").oninput = function() {
-   var val = document.getElementById("velocity_slider").value; //gets the oninput value
-   document.getElementById('velocity_value').innerHTML = val; //displays this value to the html page
-   fetch('/process_velocity_change/'+ val)
+var a = slider.value; //variable to be controlled
+var dispDiv = document.getElementById("velocity_value");
+dispDiv.innerHTML = "Set movement velocity: " + a;
+
+//function is called when slider value changes
+slider.addEventListener("change", function() {
+  a = slider.value;
+  dispDiv.innerHTML = "Set movement velocity: " + a;
+  fetch('/process_velocity_change/'+ val)
         .then(request => request.text(val))
-};
+})
+//
+//
+//document.getElementById("velocity_slider").oninput = function() {
+//   var val = document.getElementById("velocity_slider").value; //gets the oninput value
+//   document.getElementById('velocity_value').innerHTML = val; //displays this value to the html page
+//
+//};
