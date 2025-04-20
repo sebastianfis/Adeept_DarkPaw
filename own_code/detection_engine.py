ffd13041d0f205ebb95e53a1e98a5abe015d8e56
Sebastian <sebastian_fis@yahoo.de>
2025-04-20 16:43:21 +0200

    - Test of new implementation vs. old one (picamery2 vs. gstreamer)

--- own_code/detection_engine.py
+++ own_code/detection_engine.py
@@ -45,7 +45,7 @@ import argparse
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
 
 class DetectionEngine:
