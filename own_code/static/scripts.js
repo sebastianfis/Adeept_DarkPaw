c99424e212f02db64e3ecfc5bd06eb1167667a21
Sebastian <sebastian_fis@yahoo.de>
2025-04-25 22:22:38 +0200

    - Started working obn integration of new video streaming into full website

--- own_code/static/scripts.js
+++ own_code/static/scripts.js
@@ -1,54 +1,85 @@
-function Btn_Click(command) {
-    fetch('/process_button_click/'+ command)
-        .then(request => request.text(command));
+let pc = new RTCPeerConnection();
+let dataChannel;
+let socket = new WebSocket("ws://<YOUR_SERVER_IP>:8080/ws");
+
+socket.onmessage = async (event) => {
+    const msg = JSON.parse(event.data);
+    if (msg.answer) {
+        await pc.setRemoteDescription(new RTCSessionDescription(msg.answer));
+    } else if (msg.candidate) {
+        await pc.addIceCandidate(new RTCIceCandidate(msg.candidate));
+    }
 };
 
+pc.onicecandidate = ({ candidate }) => {
+    if (candidate) {
+        socket.send(JSON.stringify({ candidate }));
+    }
+};
+
+dataChannel = pc.createDataChannel("control");
+
+dataChannel.onopen = () => {
+    console.log("Data channel open");
+    callme();  // Start polling once data channel is open
+};
+
+dataChannel.onmessage = (event) => {
+    try {
+        let data = JSON.parse(event.data);
+        if (data.type === "status_update") {
+            document.getElementById("DistValue").innerHTML = data.Distance + " cm";
+            document.getElementById("RaspiCoreTemp").innerHTML = data.CPU_temp + " deg C";
+            document.getElementById("RaspiCPULoad").innerHTML = data.CPU_load + " %";
+            document.getElementById("RaspiRAMUsage").innerHTML = data.RAM_usage + " %";
+        } else {
+            console.log("Received:", data);
+        }
+    } catch (err) {
+        console.warn("Non-JSON or unknown message:", event.data);
+    }
+};
+
+(async () => {
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    socket.send(JSON.stringify({ offer }));
+})();
+
+// ----- Command Functions -----
+
+function Btn_Click(command) {
+    if (dataChannel.readyState === "open") {
+        dataChannel.send(command);
+    } else {
+        console.warn("Data channel not open");
+    }
+}
+
 function Set_actuator() {
-    var e = document.getElementById("ilazbj");
-    var act_number = e.options[e.selectedIndex].text;
-    var set_value = document.getElementById("i3r4u1").value;
-    command = 'setpwm_' + act_number + ':' + set_value;
+    let e = document.getElementById("ilazbj");
+    let act_number = e.options[e.selectedIndex].text;
+    let set_value = document.getElementById("i3r4u1").value;
+    let command = 'setpwm_' + act_number + ':' + set_value;
     Btn_Click(command);
 };
 
 function Set_velocity() {
-    var set_value = document.getElementById("velocity_slider").value;
+    let set_value = document.getElementById("velocity_slider").value;
     document.getElementById("velocity_value").innerHTML = "Set velocity: " + set_value + " %";
-    fetch('/process_velocity_change/'+ set_value)
-         .then(request => request.text(set_value))
+    Btn_Click('velocity_' + set_value);
 }
 
 function ChangeMode() {
-    var set_mode = document.getElementById("mode_select").value;
-    fetch('/process_mode_change/'+ set_mode)
-         .then(request => request.text(set_mode))
+    let set_mode = document.getElementById("mode_select").value;
+    Btn_Click('mode_select:' + set_mode);
 }
 
-function callme(){
-//This promise will resolve when the network call succeeds
-//Feel free to make a REST fetch using promises and assign it to networkPromise
-var networkPromise = fetch('/read_data')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("DistValue").innerHTML = data['Distance'] + " cm"
-    document.getElementById("RaspiCoreTemp").innerHTML = data['CPU_temp'] + " deg C"
-    document.getElementById("RaspiCPULoad").innerHTML = data['CPU_load'] + " %"
-    document.getElementById("RaspiRAMUsage").innerHTML = data['RAM_usage'] + " %"
-  });
+// ----- Periodic Status Request -----
 
-
-//This promise will resolve when 200 ms seconds have passed
-var timeOutPromise = new Promise(function(resolve, reject) {
-  // 200 millisecond delay
-  setTimeout(resolve, 200, 'Timeout Done');
-});
-
-Promise.all(
-[networkPromise, timeOutPromise]).then(function(values) {
-  callme();
-});
+function callme() {
+    if (dataChannel.readyState === "open") {
+        dataChannel.send("request_status");
+    }
+    setTimeout(callme, 200);  // Call again after 200ms
 }
-
-window.addEventListener("DOMContentLoaded", (event) => {
-    callme();
-});
