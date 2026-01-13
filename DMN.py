17848b13a08c59f6cf8ee793ba3833e9080a98b9
Sebastian <sebastian_fis@yahoo.de>
2026-01-13 14:21:17 +0100

    -Distance Sensor: Included sanity checks for edge detection -check_if_moving_target: Corrected smopthing logic.

--- DMN.py
+++ DMN.py
@@ -289,7 +289,7 @@ class DefaultModeNetwork:
         dy = max(disp_y) - min(disp_y)
         dz = max(disp_z) - min(disp_z)
 
-        logging.info(f"displacement x={dx:.2f}, y={dy:.2f}, z={dz:.2f}")
+        logging.info(f"displacement x={disp_x:.2f}, y={disp_y:.2f}, z={disp_z:.2f}")
 
         # -------------------------------------------------
         # 4. Decision + unlock
