- Corrected bug in turn around time calculation

MotionControl.py
@@ -97,7 +97,7 @@ class MotionController:
         # FIXME: The value that comes out of this is way to small!
         u_req = self.r_init * np.pi * n - self.step_length_t/2
         self.turn_around_time = u_req / self.v_t + self.t_init_t  # How many seconds it takes the robot theoretically to do n * half a turn at a given velocity
-        logger.info('calculated TA time: {.2f} s'.format(self.turn_around_time))
+        logger.info('calculated TA time: {0:.2f} s'.format(self.turn_around_time))
 
     def write_data_to_serial(self, message: str):
         message = message + ';\r\n'