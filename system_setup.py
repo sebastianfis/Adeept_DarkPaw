69d1e615eb9060d704d9b70e7f023708b1ed661b
Sebastian <sebastian_fis@yahoo.de>
2026-02-10 10:55:14 +0100

    - Updated setup

--- system_setup.py
+++ system_setup.py
@@ -77,6 +77,7 @@ def main():
     # install_packages(venv_path, [wheel_file])  # hailort .whl
 
     packages = [
+        "pip",
         "setuptools",
         "numpy==1.23.5",
         "opencv-python-headless==4.10.0.82",
