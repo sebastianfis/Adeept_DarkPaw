e7334ca00cfa3c20f139fb87b1cd0bdd3bfd8b12
Sebastian <sebastian_fis@yahoo.de>
2026-03-07 00:05:14 +0100

    - Updated Raspi camera control to fix low light behaviour

--- detection_engine.py
+++ detection_engine.py
@@ -46,7 +46,18 @@ class DetectionEngine:
         self.camera.set_controls({"AwbMode": controls.AwbModeEnum.Indoor})
         self.camera_config = self.camera.create_video_configuration(main={'size': (self.video_w, self.video_h),
                                                                           'format': 'XRGB8888'},
-                                                                    controls={'FrameRate': 30})
+                                                                    controls={
+                                                                        # 'FrameRate': 25,
+                                                                        # Lower FPS = more exposure time
+                                                                        'FrameDurationLimits': (40000, 40000),
+                                                                        # 25-25fps range
+                                                                        'AeEnable': True,
+                                                                        'AwbMode': 0,  # Auto WB
+                                                                        'Brightness': 0.1,
+                                                                        'AnalogueGain': 2.0,  # Moderate gain boost
+                                                                        'NoiseReductionMode': 2,
+                                                                        # High quality noise reduction
+                                                                    })
         self.camera.preview_configuration.align()
         self.camera.configure(self.camera_config)
 
